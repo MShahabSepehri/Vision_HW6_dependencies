2420cdf2c369f301d88ee990ba11e6115a17e3c7
ShahabSepehri <62008127+MShahabSepehri@users.noreply.github.com>
2021-07-16 14:19:50 +0430

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,7 +54,8 @@ def create_data(path, label_dic, size=(227, 227), augment=False):
     pack = list(zip(data, labels))
     pack = shuffle(pack)
     data, labels = zip(*pack)
-
+    
+    data = list(data)
     for i in range(len(data)):
         data[i] = tf.image.per_image_standardization(data[i])
 
