17eaf07870573afa80fbbdfdf4a74bed8f3f7a16
ShahabSepehri <62008127+MShahabSepehri@users.noreply.github.com>
2021-07-16 13:52:15 +0430

    Add files via upload

---
+++ augmentation_utils.py
@@ -0,0 +1,77 @@
+import numpy as np
+import PIL
+
+
+def rotate(image, angle):
+    return np.array(image.rotate(angle))
+
+
+def scale(image, factor, h, w):
+    new_size = [int(factor * h), int(factor * w)]
+    output_image = np.array(image.resize(new_size, PIL.Image.ANTIALIAS))
+
+    if factor < 1:
+        output = np.zeros(np.array(image).shape, dtype='int')
+        output_center = [int(h / 2), int(w / 2)]
+        scaled_shape = output_image.shape
+
+        output[output_center[0] - int(np.floor(scaled_shape[0]/2)): output_center[0] + int(np.ceil(scaled_shape[0]/2)),
+               output_center[1] - int(np.floor(scaled_shape[1]/2)): output_center[1] + int(np.ceil(scaled_shape[1]/2)),
+               :] = output_image
+    else:
+        output_center = [int(output_image.shape[0] / 2), int(output_image.shape[1] / 2)]
+        scaled_shape = (h, w)
+        output = output_image[output_center[0] - int(np.floor(scaled_shape[0] / 2)): output_center[0] +
+                              int(np.ceil(scaled_shape[0] / 2)),
+                              output_center[1] - int(np.floor(scaled_shape[1] / 2)): output_center[1] +
+                              int(np.ceil(scaled_shape[1] / 2)), :]
+
+    return output
+
+
+def flip(image, axis):
+    return np.flip(image, axis=axis)
+
+
+def noise(image, std):
+    image = np.array(image)
+    my_noise = np.random.normal(size=image.shape)
+    image = ((image / 255.0 + my_noise * std) * 255.0).astype('uint8')
+    return np.clip(image, 0, 255)
+
+
+def transform(image, augmentation):
+    h, w, _ = image.shape
+    image = PIL.Image.fromarray(image)
+
+    if augmentation == 'rotate':
+        angle = np.random.uniform(-10, 10)
+        return rotate(image, angle)
+
+    elif augmentation == 'scale':
+        factor = np.random.uniform(0.7, 1.4)
+        return scale(image, factor, h, w)
+
+    elif augmentation == 'flip':
+        return flip(image, 1)
+
+    elif augmentation == 'noise':
+        std = np.random.uniform(0.005, 0.05)
+        return noise(image, std)
+
+
+def augment_data(data, labels):
+    new_data = data.copy()
+    new_labels = labels.copy()
+
+    augmentations = ['rotate', 'flip', 'noise']
+
+    for i in range(len(data)):
+        for augmentation in augmentations:
+            image = data[i]
+            label = labels[i]
+
+            new_image = transform(image, augmentation)
+            new_labels.append(label)
+
+    return new_data, new_labels
---
+++ utils.py
@@ -0,0 +1,82 @@
+import numpy as np
+import cv2 as cv
+import tensorflow as tf
+from random import shuffle
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import os
+from Vision_HW6_dependencies import augmentation_utils as au
+
+
+def get_image(path, size):
+    image = cv.imread(path)
+    image = cv.resize(image, size, interpolation=cv.INTER_AREA)
+    return image
+
+
+def create_label_dic(path):
+    label_dic = {}
+    num_cls = 0
+    for subdir, _, _ in os.walk(path):
+        cls = subdir.replace(path, '')
+        if cls == '':
+            continue
+        if cls in label_dic.keys():
+            continue
+        label_dic[cls] = num_cls
+        num_cls += 1
+
+    label_reverse_dic = {}
+    for key in label_dic.keys():
+        label_reverse_dic[label_dic[key]] = key
+    return label_dic, label_reverse_dic
+
+
+def convert_to_oneHot(label, num_cls, eps=0):
+    one_hot_label = np.zeros(num_cls) + eps
+    one_hot_label[label] = 1 - (num_cls - 1) * eps
+    return one_hot_label
+
+
+def create_data(path, label_dic, size=(227, 227), augment=False):
+    data = []
+    labels = []
+    num_cls = len(label_dic.keys())
+    for subdir, _, files in os.walk(path):
+        for file in files:
+            data.append(get_image(os.path.join(subdir, file), size))
+            label = label_dic[subdir.replace(path, '')]
+            labels.append(convert_to_oneHot(label, num_cls))
+
+    if augment:
+        data, labels = au.augment_data(data, labels)
+
+    pack = list(zip(data, labels))
+    pack = shuffle(pack)
+    data, labels = zip(*pack)
+
+    new_data = []
+    for image in data:
+        new_data.append(tf.image.per_image_standardization(image))
+
+    return np.array(new_data).astype(np.float32), np.array(labels).astype(np.float32)
+
+
+def get_label_str(label_reverse_dic, label):
+    return label_reverse_dic[np.argmax(label)]
+
+
+def plot_results(history):
+    plt.figure(figsize=(5, 5))
+    plt.subplot(2, 1, 1)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.legend(['train', 'val'])
+    plt.title('Accuracy')
+    plt.subplot(2, 1, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.legend(['train', 'val'])
+    plt.title('Loss')
+    plt.tight_layout()
+    plt.show()
